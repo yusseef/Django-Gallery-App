7ea115d3a1fb9cde4df36ab1d8d32f7614a8ac1f
yusseef <yussef.raouf11@gmail.com>
2021-03-26 02:33:51 +0200

    Add models and static files

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class photos(models.Model):
+    description = models.TextField()
+    img = models.ImageField(null=False, blank= False)
+    category = models.ForeignKey(Category, on_delete= models.SET_NULL, null= True )
+
+    def __str__(self):
+        return self.description
--- photoshare/settings.py
+++ photoshare/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+MEDIA_ROOT = BASE_DIR / 'static/images'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
