88d0d8c5afa8a9779576da30aaf6788bd9ce39ab
yusseef <yussef.raouf11@gmail.com>
2021-03-27 06:21:15 +0200

    render the database

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from .models import Category, photos
 # Create your views here.
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+    categories = Category.objects.all()
+    images = photos.objects.all()
+    context = {'categories': categories, 'images': images}
+    return render(request, 'photos/gallery.html', context)
 
 def viewphoto(request, pk):
-    return render(request, 'photos/photo.html')
+    img = photos.objects.get(id=pk)
+    return render(request, 'photos/photo.html',{'img': img})
 
 def addphoto(request):
     return render(request, 'photos/add.html')
